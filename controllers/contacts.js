11686d9ba05d3f4a1eb1214892118432f8257931
Fuad J Torres <fuadjtorres99@gmail.com>
2025-09-27 22:38:17 -0500

    i should close the database

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,12 +1,15 @@
 const db_module = require('../db/db'); 
 
 // utilities for now:
-async function get_contact(name_f) { try {
+async function get_contact(name_f) { 
+    
+    try {
 
     const db =       await db_module.get_db('CSE');               // logs in terminal
     const contacts = await db.collection('contacts');                console.log("Collection objects: " + contacts);
     const json =     await contacts.findOne({name_f: name_f});       console.log("found: " + json);
 
+    db_module.close_db();                                            
     return json
 
 }   catch (err) { console.error(err); return {name:err} } }
